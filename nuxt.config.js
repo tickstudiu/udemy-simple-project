3cd1f561563ca416f12dfa4306d8c4d125e531fa
Wanchalerm Suksawat <tickstudiu@gmail.com>
2021-09-01 01:59:51 +0700

    update vuelidate

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,6 +38,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/services.ts',
+    '~/plugins/vuePlugins.ts',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ plugins/vuePlugins.ts
@@ -0,0 +1,11 @@
+/**
+ * Define Vue global plugins
+ */
+
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+
+/**
+ * Vue Plugin
+ */
+Vue.use(Vuelidate)
