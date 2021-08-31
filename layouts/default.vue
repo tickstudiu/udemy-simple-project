61bc6b6635248557e01440a443e8646c24f26cc8
Wanchalerm Suksawat <tickstudiu@gmail.com>
2021-08-31 21:03:44 +0700

    update port and update layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar />
+    <div>
+      <Nuxt />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Navbar from '@/components/layouts/Navbar.vue'
+import Footer from '@/components/layouts/Footer.vue'
+
+export default Vue.extend({
+  components: {
+    Navbar,
+    Footer,
+  },
+})
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 8080,
   },
 
   /**
